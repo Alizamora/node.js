05451395b8e6b91d992b53187e19a57e3d361417
Alisson Zamora <alison.zam18@gmail.com>
2019-06-26 17:09:30 -0600

    implementación del sort

--- Tarea#1/Back/get.js
+++ Tarea#1/Back/get.js
@@ -2,25 +2,59 @@ const fs = require('fs');
 const { end, body, coincidence, isPath } = require("./utilitary-functions");
 const brands = require("./brands.json");
 const cars = require("./cars.json");
+const items = 10;
 
-function CSV(array){
-	let h = Object.keys(array[0]).join(',')+ "\n";
+function query(req, myVar) {
+	let reg = new RegExp(`\\??&?(${myVar})=(.+)&?`, 'g')
+		.exec(req.url.split('%20').join(' '));
+	return reg ? reg[2].split('&')[0] : reg;
+}
+
+function sort(prop, array) {
+	function weightStr(str) {
+		return (str + '').split('')
+			.map((l, i) => l.charCodeAt() / (i + 1) ** (i + 1))
+			.reduce((a, b) => a + b);
+	}
+	return array[0][prop] ?
+		[].concat(array).sort((a, b) =>
+			weightStr(a[prop]) - weightStr(b[prop])) : array;
+}
+
+function CSV(array) {
+	let h = Object.keys(array[0]).join(',') + "\n";
 	let b = "";
-	array.forEach( o => {
-		b += Object.keys(o).map(k => o[k]).join(',')+ "\n";
+	array.forEach(o => {
+		b += Object.keys(o).map(k => o[k]).join(',') + "\n";
 	});
 	h += b;
 	return h;
 }
 
-
 function GET(req, res) {
 	if (req.url === '/api/v1/cars/report') {
 		console.log(CSV(cars));
 		res.end(CSV(cars));
-	}else if(isPath(req.url, '/api/v1/cars')){
-		res.end('Holi');
-	}else {
+	} else if (isPath(req.url, '/api/v1/cars')) {
+		const page = +query(req, 'page');
+		const sortBy = query(req, 'sortBy');
+		const search = query(req, 'search');
+		let array = [].concat(brands)
+			.splice(
+				Math.abs(page ? page - 1 : 0) * items, items);
+		if (search) {
+			array = array.map(o => {
+				for (let key in o) {
+					o[key] += '';
+					if (o[key].indexOf(search) > -1) return o;
+				}
+			});
+		}
+		if (sortBy) {
+			array = sort(sortBy ? sortBy : 'id', cars);
+		}
+		end({ res, d: array });
+	} else {
 		end({ res, s: false, e: 'End Point not valid', st: 500 });
 	}
 }
