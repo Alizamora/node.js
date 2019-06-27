4a69a7795bbfdd4ef22eb212e65ce25746835124
Alisson Zamora <alison.zam18@gmail.com>
2019-06-26 23:00:54 -0600

    subo la funcionalidad de delete

--- Tarea#1/Back/delete.js
+++ Tarea#1/Back/delete.js
@@ -3,8 +3,17 @@ const { end, body, coincidence } = require("./utilitary-functions");
 const brands = require("./brands.json");
 
 function DELETE(req, res) {
-
+  body({ req }, d => {
+		let obj = cars[d.id] ? +cars[d.id] : null;
+    if(obj) {   
+      cars.splice(obj.id, 1);
+      cars.forEach((c, i) => c.id = i);
+      fs.writeFile('./cars.json', JSON.stringify(cars), (err) => {
+        if (err) throw new Error(err);
+      });
+      end({ res, d: cars });
+    } else end({res, e: 'not valid id'});    
+  });
 }
-
 
 module.exports = DELETE;
